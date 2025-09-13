c692cf48b403e8705fb9e61d4f13b89d61a5e7e0
Prince Subedi <subediprince05@gmail.com>
2025-09-13 22:28:36 +0545

    Day 2

---
+++ chapter 6/conditional01.py
@@ -0,0 +1,10 @@
+x=int(input("Enter Your age:"))
+
+
+if x > 20:
+    print("x is greater than 20")
+elif x > 10:   # <-- this is like "else if"
+    print("x is greater than 10 but less than or equal to 20")
+else:
+    print("x is 10 or less")
+
