4388be3b02615e4d648c5058a2be9d0c8339c81d
Prince Subedi <subediprince05@gmail.com>
2025-09-13 21:24:23 +0545

    Day 2

---
+++ chapter 5/problem03.py
@@ -0,0 +1,20 @@
+d={}
+
+name=input("Enter friends name:")
+lan=input("enter fav language:")
+d.update({name:lan})
+
+name=input("Enter friends name:")
+lan=input("enter fav language:")
+d.update({name:lan})
+
+name=input("Enter friends name:")
+lan=input("enter fav language:")
+d.update({name:lan})
+
+name=input("Enter friends name:")
+lan=input("enter fav language:")
+d.update({name:lan})
+
+
+print(d)
---
+++ chapter 5/problem1.py
@@ -0,0 +1,9 @@
+words = {
+    "shayog":"Help",
+    "khanu":"eat",
+    "piunu":"drink"
+}
+
+word=input("enter the word you want meaning of:")
+
+print(words[word])
