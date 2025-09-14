34131452e504b471e1e24c98b75650df5317eac2
Prince Subedi <subediprince05@gmail.com>
2025-09-14 19:57:52 +0545

    Day 3

---
+++ chapter 6/problem7.py
@@ -0,0 +1,7 @@
+post=input("Enter Your Post:")
+
+if("Prince".lower() in post.lower()):
+    print("This Post is Talking about Prince ")
+
+else:
+    print("This POst is NOt talking about prince")    
