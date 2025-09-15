102ad121a4f7c8805f931822cd829817e573c34f
Prince Subedi <subediprince05@gmail.com>
2025-09-15 21:31:55 +0545

    Day 3

--- chapter 9/Practice/p3.py
+++ chapter 9/Practice/p3.py
@@ -1,1 +1,13 @@
-for i in range(2,21)
+def  generatetable(n):
+    table= ""
+    for i in range(1,11):
+        table += f"{n} x {i} = {n*i}\n"
+
+    with open(f"tables/table_{n}.txt","w") as f :
+        f.write(table)    
+
+
+
+# for how many tables you can generate 
+for i in range(2,21):
+    generatetable(i)
