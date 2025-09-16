aeb62d4bbec4dc4f0eb517df7b79a56ef0495c7a
Prince Subedi <subediprince05@gmail.com>
2025-09-16 10:00:26 +0545

    Day 4

---
+++ chapter 10/011_class.py
@@ -0,0 +1,13 @@
+class Employee:
+    salary=2000000
+    language="py"
+
+a=Employee()
+a.name="Prince"
+print(a.name,a.salary,a.language)
+
+b=Employee()
+b.name="Pratap"
+print(b.name,b.salary,b.language)
+
+# here name is object attribute and salary and language is class attributrs as they directly belongs to the class 
---
+++ chapter 10/01_class.py
@@ -0,0 +1,7 @@
+class Employee:
+    name="prince"
+    age=20
+    salary=120000
+
+a=Employee()
+print(f"Name is : {a.name},,",f"Age is : {a.age}")    
