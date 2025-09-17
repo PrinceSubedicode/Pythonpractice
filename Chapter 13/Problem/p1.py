db0545f021a11a131ab4eb959c6457692b13c13d
Prince Subedi <subediprince05@gmail.com>
2025-09-17 13:02:12 +0545

    Day 5

---
+++ Chapter 13/Problem/p1.py
@@ -0,0 +1,6 @@
+name=input("Enter Your Name:")
+marks=int(input("Enter Marks:"))
+phone=int(input("Phone Number:"))
+
+s="The name of the student is {},his marks are {} and his  phone number is {}".format(name,marks,phone)
+print(s)
